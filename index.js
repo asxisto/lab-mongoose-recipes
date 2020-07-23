f403a33a7c6e4c814bdb5e5492eac169b1473fe1
asxisto <asxisto@gmail.com>
2020-07-23 19:05:18 +0100

    lab completed: iterations 1-6! :)

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,6 +22,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Chocolate Cake',
+      level: 'Amateur Chef',
+      ingredients: ['chocolate', 'cake'],
+      cuisine: 'International'
+    });
+  })
+  .then(recipe => {
+    console.log('Recipe created: ', recipe.title);
+    return Recipe.insertMany(data);
+  })
+  .then(newRecipes => {
+    for (let i = 0; i < newRecipes.length; i++) {
+      console.log('New recipe added:', newRecipes[i].title);
+    }
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then(updatedRecipe => {
+    console.log(updatedRecipe.title, 'successfully updated.');
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(deletedRecipe => {
+    console.log('Recipe successfully deleted.');
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Disconnected from MongoDB');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
